fdd458348d23a444dbd98c2e84ae39338f878b0a
jithu <jithusoe@gmail.com>
2023-01-21 01:57:47 +0530

    second time update server url

--- client/script.js
+++ client/script.js
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chitti-robot.onrender.com', {
+    const response = await fetch('https://chitti-robot.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
